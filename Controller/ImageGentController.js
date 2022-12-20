964bc7ba97ef33f63233c5daaf9dcbf17819172b
Dibyank kumar singh <76806222+dibyank-singh@users.noreply.github.com>
2022-12-20 13:32:15 +0530

    APi integration is done

--- Controller/ImageGentController.js
+++ Controller/ImageGentController.js
@@ -1,8 +1,36 @@
+const { Configuration, OpenAIApi } = require("openai");
 
-const ImageGeneratorAI=(req, res)=>{
-    res.status(200).json({
-        message:"Success"
-    })
+const configuration = new Configuration({
+  apiKey: "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",
+});
+const openai = new OpenAIApi(configuration);
+
+const ImageGeneratorAI= async(req, res)=>{
+     const {prompt, size}=req.body;
+     console.log(prompt,size)
+     const imagesize= size === "small" ? '256x256' : size ==="medium"? 
+      '512x512': '1024x1024';
+    try{
+        console.log("checkingggg")
+        const response= await openai.createImage({
+            prompt:prompt,
+            n:1,
+            size:imagesize
+        })
+       console.log(response)
+       const ImageUrl= response.data.data[0].url
+
+        res.status(200).json({
+            message:"Success",
+            data:ImageUrl
+        })
+        console.log("it hits here..")
+    }catch(err){
+        res.status(400).json({
+            error:err,
+            message:"Sorry, We can't generate this image..."
+        })
+        console.log("it hits here.. errrr mee")
+    }
 }
-
 module.exports={ImageGeneratorAI}
--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 const express= require("express")
 const env= require("dotenv")
+const bodyParser = require('body-parser');
 const port=process.env.PORT || 5000
 
-const app=express()
+const app = express();
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
 
 app.use("/openai", require("./Routes/ImageGentRoute"))
-
-
 
 app.listen(port, ()=>{
     console.log(`server is runnig at ${port}`)
