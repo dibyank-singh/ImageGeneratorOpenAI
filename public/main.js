3429dc621f7b459df1212007dc52225ee3f9416b
Dibyank kumar singh <76806222+dibyank-singh@users.noreply.github.com>
2022-12-23 15:03:28 +0530

    all functionality code added

--- public/main.js
+++ public/main.js
@@ -1,7 +1,9 @@
 
+
 function OnSubmit(e){
   e.preventDefault();
 
+  document.querySelector('#image-main').src=''
 const prompt= document.querySelector('#Img-descrpt').value
 const size=document.querySelector('#size').value
 
@@ -10,6 +12,41 @@ if(!prompt){
 }
   ImagegeneratorApicall(prompt,size)
   
+}
+
+async function ImagegeneratorApicall(prompt, size){
+
+    try{
+      showspinner()
+       const response= await fetch("/openai/imagegeneratorai", {
+        method:'POST',
+        headers:{
+            'Content-Type':'application/json'
+        },
+        body: JSON.stringify({
+            prompt,
+            size
+        })
+       })
+
+       const data=await response.json()
+       const imageurl=data.data
+       document.querySelector('#image-main').src=imageurl
+       removespinner()
+       document.querySelector('#text-gen').textContent="Generated Image-->"
+       
+    }catch(err){
+
+    }
+}
+
+function showspinner(){
+  
+  document.querySelector('.spinner').classList.add('show')
+  
+}
+function removespinner(){
+  document.querySelector('.spinner').classList.remove('show')
 }
 
 document.querySelector('#main-form').addEventListener('submit', OnSubmit)
